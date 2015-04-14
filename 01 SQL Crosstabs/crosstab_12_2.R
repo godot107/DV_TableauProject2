50360367b7d5cb89d62350ed18e290b27a19f5fc
Willie Man <man.willieman@yahoo.com>
2015-04-14 14:09:50 -0500

    no message

--- 01 SQL Crosstabs/crosstab_12_2.R
+++ 01 SQL Crosstabs/crosstab_12_2.R
@@ -1,3 +1,7 @@
+require("jsonlite")
+library("RCurl")
+require(dplyr)
+
 # one requirement for number 12 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "select ID, RACE, BWT, last_value(max_BWT) 
@@ -8,4 +12,4 @@ from
 OVER (PARTITION BY RACE) max_BWT 
 FROM BABY2) 
 order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df) 
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); head(df) 
--- 01 SQL Crosstabs/crosstab_9a.R
+++ 01 SQL Crosstabs/crosstab_9a.R
@@ -1,6 +1,10 @@
+require("jsonlite")
+library("RCurl")
+require(dplyr)
+
 # one requirement for number 9
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
 "SELECT ID , RACE, BWT, rank()
 OVER (PARTITION BY RACE order by BWT) WEIGHT_RANK 
 FROM BABY2 order by 2,4 desc " 
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(df) 
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); head(df) 
